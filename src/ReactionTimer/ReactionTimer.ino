1422fec87b00724b8e052b294ad514b2333fed89
ThorinDev <c.nethercott@icloud.com>
2016-02-29 13:04:10 +0000

    Updated some stuff
    Should be ready for rollout!

--- src/ReactionTimer/ReactionTimer.ino
+++ src/ReactionTimer/ReactionTimer.ino
@@ -1,11 +1,24 @@
+/*
+  Chris Nethercott
+  Reaction Timer
+*/
+
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 const int switchPin = 2;
-const int ledPin = 12;
+const int ledPinA = 3; // Red
+const int ledPinB = 4; // Red
+const int ledPinC = 5; // Red
+const int ledPinD = 6; // Red
+const int ledPinE = 7; // Red
+const int ledPinF = 8; // Green
+
 int randomTimeMin = 5;
 int randomTimeMax = 20;
+int ledDelayTime = 1500;
+
 boolean lastButton = LOW;
 boolean currentButton = LOW;
 boolean Started = false;
@@ -16,14 +29,27 @@ long randomTime;
 float elapsedTime;
 
 void setup() {
-  pinMode(switchPin, INPUT);
-  pinMode(ledPin, OUTPUT);
+  pinModes();
+  
   Serial.begin(9600);
+  
   randomSeed(analogRead(-1));
+  
   lcd.begin();
   lcd.backlight();
   lcd.print(" Reaction Timer ");
+  
   digitalWrite(switchPin, HIGH);
+}
+
+void pinModes() {
+  pinMode(switchPin, INPUT);
+  pinMode(ledPinA, OUTPUT);
+  pinMode(ledPinB, OUTPUT);
+  pinMode(ledPinC, OUTPUT);
+  pinMode(ledPinD, OUTPUT);
+  pinMode(ledPinE, OUTPUT);
+  pinMode(ledPinF, OUTPUT);
 }
 
 boolean debounce(boolean last) {
@@ -53,31 +79,52 @@ void loop() {
   if(Started == false && timer == true) {
     Stop();
     timer = false;
-  }
- 
+  } 
 }
+
 void Random() {
   randomTime = random(randomTimeMin,randomTimeMax);
   randomTime = randomTime*1000;
 
   lcd.clear();
-  lcd.print(" Reaction Timer");
-  digitalWrite(ledPin, HIGH);
+  lcd.print("Reaction Timer");
+  digitalWrite(ledPinG, HIGH);
   delay(100);
-  digitalWrite(ledPin, LOW);
+  digitalWrite(ledPinG, LOW);
   delay(1000);
   lcd.setCursor(0,1);
   lcd.print("Get Ready!");
   Serial.print("Random Time: ");
   Serial.println(randomTime);
+  ledSequence();
   delay(randomTime);
   Start();
 }
 
+void ledSequence() {
+  digitalWrite(ledPinA, HIGH);
+  delay(ledDelayTime);
+  digitalWrite(ledPinB, HIGH);
+  delay(ledDelayTime);
+  digitalWrite(ledPinC, HIGH);
+  delay(ledDelayTime);
+  digitalWrite(ledPinD, HIGH);
+  delay(ledDelayTime);
+  digitalWrite(ledPinE, HIGH);
+  delay(ledDelayTime);
+}
 
 void Start() {
   startTime = millis();
-  digitalWrite(ledPin, HIGH);
+  allLedsLow();
+}
+
+void allLedsLow() {
+  digitalWrite(ledPinA, LOW);
+  digitalWrite(ledPinB, LOW);
+  digitalWrite(ledPinC, LOW);
+  digitalWrite(ledPinD, LOW);
+  digitalWrite(ledPinE, LOW);
 }
 
 void Stop() {
@@ -90,6 +137,7 @@ void Stop() {
   lcd.print("Time: ");
   lcd.print(elapsedTime);
   lcd.print("ms");
+  
   lcd.setCursor(0,1);
   if(elapsedTime < 0.5) {
     lcd.print("You are fast!");
@@ -103,6 +151,6 @@ void Stop() {
   else {
     lcd.print("Loser!");
   }
-  digitalWrite(ledPin, LOW);
-   
+  
+  allLedsLow();   
 }
